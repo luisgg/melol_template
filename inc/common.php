9aaf44c9733afb56e9022fafd71da696160e0612
Luis Garcia Gisbert <luisgg@gmail.com>
2016-01-14 19:13:34 +0100

    added css

--- inc/common.php
+++ inc/common.php
@@ -5,6 +5,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
 function sys_session_test(){
     session_name("MELOLSESSION");
     session_start();
--- index.php
+++ index.php
@@ -6,18 +6,47 @@ and open the template in the editor.
 -->
 <?php
         include 'inc/common.php';
-        if (! session_is_started()) {
+        if (! sys_session_test()) {
             header("Location: login.php");
         }
 ?>
+
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>MELOL</title>
+	<link media="all" href="css/style.css" rel="stylesheet" type="text/css"></link>
+        <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+        <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+    <div class="container">
+        <div id="wrapper">
+            <div id="header">
+                <div id="logo">
+                    <img src="img/logo_web.png"></img>
+                </div>
+                <div id="title">
+                    Manager Emulator LOL
+                </div>
+
+            </div>
+            <nav class="melolbar">
+                <div><a href="index.php?action=action1">Inicio</a></div>
+                <div><a href="index.php?action=action2">Altas</a></div>
+            </nav>
+            <div id="content">
+            <?php
+            // controlador.php se encargara de mostrar el 'contenido' correspondiente
+                include('./controlador.php');
+            ?>
+            </div>
+            <div id="footer">
+            <div class="pull-left"><kbd>#MELOL</kbd></div>
+                <div id="13" class="pull-right"><kbd>ASIR (2016)</kbd></div>
+            </div>
+        </div>
+    </div>
     </body>
-</html>
+</html> 
